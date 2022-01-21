4777808ffafdf34dad955b3d3297718ceb5acb73
python-programing <79223364+python-coding-404@users.noreply.github.com>
2022-01-21 17:10:27 +0400

    kks

--- static/script.js
+++
@@ -1,9 +0,0 @@
-let lol = document.querySelector(".l")
-let lo = document.querySelector(".lo")
-let l = document.querySelector("dialog")
-lol.addEventListener("click", function(){
-    l.show()
-})
-lo.addEventListener("click", function(){
-    l.close()
-})
